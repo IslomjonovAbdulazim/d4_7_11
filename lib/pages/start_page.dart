0d658f0f7942ffba059f08446aac8087af521601
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-28 16:49:03 +0500

    imp

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StartPage extends StatelessWidget {
@@ -10,18 +11,55 @@ class StartPage extends StatelessWidget {
       body: Stack(
         children: [
           // Background Image
+          Image.asset("assets/background.png"),
 
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.all(20),
               child: Center(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     // Robot
+                    Image.asset("assets/robot.png"),
 
                     // Texts (2x)
+                    SizedBox(height: 20),
+                    Text(
+                      "The Most Trusted And Fast Chatbot Ever",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      "Budddy is the most trusted and fast chatbot ever made on internet.",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xff949BA5),
+                        fontWeight: FontWeight.w300,
+                        height: 1.2,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
 
                     // Buttons
+                    SizedBox(height: 24),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: CupertinoButton(
+                            onPressed: () {},
+                            color: Color(0xff212226),
+                            child: Text(
+                              "Sign Up",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
